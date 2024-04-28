a819598121410c3ef923fdeac16bcd559be7bacc
Faten Habbal <107545770+FatenHabbal@users.noreply.github.com>
2024-04-28 21:10:24 +0300

    lesson11.py

---
+++ Sibervatan_Ders/lesson11.py
@@ -0,0 +1,34 @@
+#basamakların aynı olup olmadığını ölçen program asıl yer 4. satır
+def sayi_kontrol(sayi):
+        return 0
+    
+A = [233,45,777,81,99999,36,90,88,11,61,513]
+for sayi in A:
+    if sayi_kontrol(sayi):
+        print(f"{sayi}:basamakları aynı")
+    else:
+        print(f"{sayi}:basamakları farklı")
+
+print("#######################################################################")
+
+#CLASS SİSTEMİ
+liste=[10,20,30]
+print(type(liste))
+#listenin tipini sorguladım
+
+class Person:
+        return 2024- self.dogum
+p1 = Person(name="ali" ,lname="koç",adress="100.yıl",dogum=2000)
+print(p1)
+print("benim adım" ,p1.name)
+print("benim adım" ,p1.name, "benim soyadım",p1.lname,"benim adresim",p1.adress,"doğum yılım",p1.dogum)
+
+p2 = Person(name="nisa" ,lname="agaccioglu",adress="karabuk merkez",dogum=2008)
+print(p2)
+print("benim adım" ,p2.name)
+print("benim adım" ,p2.name, "benim soyadım",p2.lname,"benim adresim",p2.adress,"doğum yılım",p2.dogum)
+
+p1.intro()
+p2.intro()
+p1.yashesabı()
+p2.yashesabı()
